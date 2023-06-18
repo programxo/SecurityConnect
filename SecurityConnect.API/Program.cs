3ca59688bf31cb9c805c700c17f4263e57c1e59a
Schamil Abubakarov <shamil.abubakr@proton.me>
2023-06-18 13:47:21 +0200

    9. Creating WebApp (with Authentication API)

--- SecurityConnect.API/Program.cs
+++ SecurityConnect.API/Program.cs
@@ -1,13 +1,19 @@
 ﻿using SecurityConnect.WebApp;
 using SecurityConnect.Application;
 using SecurityConnect.Infrastructure;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+
     builder.Services
         .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddMudServices();
 }
 
 var app = builder.Build();
@@ -15,10 +21,16 @@ var app = builder.Build();
     // Configuring global exception handler
     app.UseExceptionHandler("/error");
 
+    app.UseStaticFiles();
+    app.UseRouting();
+
     // Configure the HTTP request pipeline.
     app.UseHttpsRedirection();
     app.UseAuthentication();
     app.UseAuthorization();
+    app.MapBlazorHub();
     app.MapControllers();
+    app.MapFallbackToPage("/_Host");
+
     app.Run();
 }
